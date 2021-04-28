558770fabece29273bbcdd6cc15724311bcfe377
Lucre <josielindamaldonado@gmail.com>
2021-04-28 13:43:28 -0400

    added mainHB, util files, package.json scripts.

--- seeds/seed.js
+++ seeds/seed.js
@@ -12,12 +12,12 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const post of postData) {
-    await post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const post of postData) {
+  //   await post.create({
+  //     ...post,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
