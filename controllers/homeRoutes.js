895d96ecdc46e45bcd545ac959cf12feb1804722
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-04-28 13:17:44 -0500

    added to homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const { Post, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", (req, res) => {
+  res.render("main");
+});
+
+// If user is logged in, redirect to homepage, otherwise render login
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/homepage");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/homepage", (req, res) => {
+  res.render("homepage");
+});
+
+router.get("/users", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Post,
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("profile", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
