c67defa164f39174c1ee0a394838968d4647ad52
Lucre <josielindamaldonado@gmail.com>
2021-04-29 18:56:25 -0400

    added if else for URL request based on user input.

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,24 @@
 $(document).ready(() => {
   // VARIABLES -----------------------------------------------------------------------
-  var requestURL = "http://www.boredapi.com/api/activity?";
+  var requestURL = "http://www.boredapi.com/api/activity?participants=4";
+  var participants = 1;
+  var price = 0;
 
 
   // ARRAYS --------------------------------------------------------------------------
   var activitiesData = [];
-  
+
 
   // API bored fetch to get data
   function getAPI() {
+      if (price == "yes") {
+        requestURL = "http://www.boredapi.com/api/activity?participants=1&price=0.0";
+      } else if (price == "no") {
+        requestURL = "http://www.boredapi.com/api/activity?participants=1";
+      } else {
+        requestURL = "http://www.boredapi.com/api/activity?participants=" +
+        participants;
+      }
       for (var i = 0; i < 3; i++) {
           fetch(requestURL)
           .then(function (response) {
@@ -23,4 +33,6 @@ $(document).ready(() => {
     }
     
     getAPI();
+
+    
 });
