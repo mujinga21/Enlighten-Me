de5f45762ffbc504b2183e099a4040644a50ef89
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-04-30 14:46:19 -0400

    comments on api

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 // const withAuth = require("../../utils/auth");
 
+//CREATE a post
 router.post('/', async (req, res) => {
   try {
     const postData = await Post.create({
@@ -13,6 +14,7 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+//GET a single post
 router.get('/:id', async (req, res) => {
   console.log("hello world");
   try {
@@ -28,6 +30,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+//GET all posts
 router.get('/', async (req, res) => {
   console.log("hello world");
   try {
@@ -43,44 +46,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.put('/:post_id', (req, res) => {
-  Post.update(
-    {
-      title: req.body.title,
-      body: req.body.body,
-    },
-    {
-      where: {
-        id: req.params.post_id,
-      },
-    }
-  )
-    .then((updatedPost) => {
-      res.json(updatedPost);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
-});
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const postData = await Post.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!postData) {
-      res.status(404).json({ message: "No post found with this id!" });
-      return;
-    }
-
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,16 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const userData = await User.create(req.body);
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
+//CREATE a sign up
 router.post("/signup", async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -24,7 +15,7 @@ router.post("/signup", async (req, res) => {
     res.status(400).json(err);
   }
 });
-
+//Find a user and login
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({
@@ -57,7 +48,7 @@ router.post("/login", async (req, res) => {
     res.status(400).json(err);
   }
 });
-
+// logout user
 router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
