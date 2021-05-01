b4570655caa4487c49aa206997250f558c71d874
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-01 12:47:34 -0500

    styling to posts

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,19 @@
+const hb = require("handlebars");
+const moment = require("moment");
+
+hb.registerHelper("dateFormat", (date, options) => {
+  const formatToUse =
+    (arguments[1] && arguments[1].hash && arguments[1].hash.format) || "LLLL";
+  return moment(date).format(formatToUse);
+});
+
 const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
